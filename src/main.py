8c1a5ed35fd90cbba4aa84cf203825aa2a19b8c6
kemal576 <kemal.sahin.2@deliveryhero.com>
2022-07-25 12:48:01 +0300

    request operation moved to a method

--- src/main.py
+++ src/main.py
@@ -1,31 +1,41 @@
 import json
 import requests
+from requests.exceptions import HTTPError
+
 from src.config import Settings
 from src.holiday import Holiday
 
 
-def main():
+def get_holidays() -> list:
     url = Settings.API_URL
     key = Settings.API_KEY
-    # country = input("Type the code of the country whose holidays you want to list? like US,FR,TR :")
     country = "TR"
     year = 2021
 
-    res = requests.get(url=url + "/holidays",
-                       params={"key": key, "country": country, "year": year})
+    res = requests.get(url=f"{url}/holidays", params={"key": key, "country": country, "year": year})
 
-    if res.status_code == 200:
-        response = json.loads(res.content)
-        holidays_json = response["holidays"]
-        holiday_list = [Holiday(**holiday) for holiday in holidays_json]
+    try:
+        res.raise_for_status()
+    except HTTPError as e:
+        print(f"An HTTP error occurred. Status Code: {e.response.status_code}")
+        raise
 
-        print("%d holidays found for %s:" % (len(holiday_list), country))
-        for holiday in holiday_list:
+    response = json.loads(res.content)
+    holidays_json = response["holidays"]
+    holiday_list = [Holiday(**holiday) for holiday in holidays_json]
+
+    print("%d holidays found for %s:" % (len(holiday_list), country))
+    return holiday_list
+
+
+def main():
+    try:
+        holidays = get_holidays()
+        for holiday in holidays:
             print(holiday)
-        exit(1)
 
-    print(f"An error occurred. Status Code: {res.status_code}")
-    exit(1)
+    except HTTPError:
+        exit()
 
 
 if __name__ == '__main__':
